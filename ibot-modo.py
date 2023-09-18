d66e22a38179bfb141455a39f3ea0e8a206c1d45
Slendy_Milky <slendymilky@gmail.com>
2023-09-18 12:44:16 +0200

    Update ibot-modo.py

--- ibot-modo.py
+++ ibot-modo.py
@@ -13,12 +13,6 @@ DEBUG = os.getenv('DEBUG') == 'true'
 OPENAI_LEVEL_NUM = 25
 logging.addLevelName(OPENAI_LEVEL_NUM, "OPENAI")
 
-def openai(self, message, *args, **kws):
-    if self.isEnabledFor(OPENAI_LEVEL_NUM):
-        self._log(OPENAI_LEVEL_NUM, message, args, **kws) 
-
-logging.Logger.openai = openai
-
 # Configure logging
 logging.basicConfig(level=logging.DEBUG if DEBUG else logging.INFO,
                     format='%(asctime)s %(message)s - %(levelname)s',
@@ -27,7 +21,7 @@ logging.basicConfig(level=logging.DEBUG if DEBUG else logging.INFO,
                               logging.FileHandler("modo_log.txt")])
 
 # Instantiate the bot
-bot = commands.Bot(command_prefix="§")
+bot = commands.Bot(command_prefix="§", intents=nextcord.Intents.all())
 
 if DEBUG:
     @bot.event
@@ -41,38 +35,41 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    logging.info(f"Raw message content: {message.content}")
-    logging.info(f"System message content: {message.system_content}")
-    for embed in message.embeds:
-        logging.info(f"Embed: {embed.to_dict()}")
-    for attachment in message.attachments:
-        logging.info(f"Attachment: {attachment.url}")
-    
+    logging.info(message.content)
+
     if message.author == bot.user:
         logging.info("Message sent by the bot. Skipping moderation...")
         return
-    if message.channel.category is not None and message.channel.category.name[0] in ["🟢", "🔵", "🟡"]:
-        if message.content.strip():
+    elif message.content.strip():
+        if message.channel.category is not None and message.channel.category.name.startswith(("🟢", "🔵", "🟡")):
             logging.info(f"Sending message for moderation: {message.content}")
             response = openai.Moderation.create(input=message.content) 
             output = response['results'][0]
-
+        
             flagged_categories = [category for category, flagged in output['categories'].items() if flagged]
 
             if flagged_categories:
+                # Create a link to the message
+                message_link = f"https://discord.com/channels/{message.guild.id}/{message.channel.id}/{message.id}"
+
                 mod_channel = bot.get_channel(1153256725525315656)
-                embed = nextcord.Embed(title="Flagged Message", color=0xff0000)
-                embed.add_field(name="Author", value=message.author.name, inline=False)
+                embed = nextcord.Embed(title="Message problématique", url=message_link, color=0xff0000)
+                embed.timestamp = message.created_at
+
+                embed.add_field(name="Auteur", value=message.author.name, inline=True)
+                embed.add_field(name="ID", value=message.author.id, inline=True)
+                embed.add_field(name="Catégories de modération", value=', '.join(flagged_categories), inline=True)
                 embed.add_field(name="Message", value=message.content, inline=False)
-                embed.add_field(name="Flagged Categories", value=', '.join(flagged_categories), inline=False)
+
+                embed.set_footer(icon_url=message.author.avatar.url, text=message.author.name)
+
                 logging.warning(f"Flagged message: {message.content}, Flagged categories: {', '.join(flagged_categories)}")
                 await mod_channel.send(embed=embed)
-
+                
         else:
             logging.info("Received an empty message. Skipping moderation...")
     else:
         logging.info("Message doesn't match category. Skipping moderation...")
-
 
 # Run the bot
 bot.run(BOT_TOKEN)
